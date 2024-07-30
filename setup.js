4e0e86c8176e3241313373643d28d08711115647
dalveer <dalveerkumar249>
2024-07-30 13:22:36 +0530

    add two number

--- setup.js
+++ setup.js
@@ -1,1 +1,13 @@
-console.log("hello javascript world")
+// addNumbers.js
+
+// Function to add two numbers
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+// Test the function
+let number1 = 5;
+let number2 = 7;
+let result = addTwoNumbers(number1, number2);
+
+console.log(`The sum of ${number1} and ${number2} is ${result}`);
