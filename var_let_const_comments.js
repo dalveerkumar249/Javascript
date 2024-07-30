84305b8994282d41fa66961d2281416073c79c02
dalveer <dalveerkumar249>
2024-07-30 14:31:03 +0530

    javascript variables

---
+++ var_let_const_comments.js
@@ -0,0 +1,21 @@
+const accountId = 1234
+
+let accountName = "dalveer"
+
+var accountEmail = "dalveer@google.com"
+
+accountName = "dilabar"
+accountEmail = "github@gmail.com"
+
+//accountId =     not allowed
+var name;
+
+console.log(accountId);
+console.log(accountName);
+console.log(accountEmail);
+console.log(name)  // this give undefined because we dont assign a value to the variable name 
+
+
+// note  only let and var can change const variable is not changed 
+
+//  for one line comment use the // and for the multiple line comments use the /* your code */  . 
